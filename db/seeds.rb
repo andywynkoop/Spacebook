febf5a252c1cd70d03fc99628b11f17075561ca2
andywynkoop <andywynkoop@gmail.com>
2018-04-09 22:49:51 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,7 @@ User.create(
  profile_img_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/19748628_10105116560528514_928545978395585021_n.jpg?_nc_cat=0&oh=4300167ae91a3dbb0e9d59cacc387c50&oe=5B5C4ABD",
  cover_photo_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/19732187_10105116555104384_5365679968007041198_n.jpg?_nc_cat=0&oh=7610a6e845d1fb2020cd25492ce5f340&oe=5B288001",
  bio: "Keep your logic above your sentiments. Or else you're headed for regrets.",
- sex: "Male">
+ sex: "Male"
 )
 
 User.create(
@@ -64,7 +64,7 @@ User.create(
  profile_img_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/20768110_10212617292941704_3159586047364940712_n.jpg?_nc_cat=0&oh=f3aa038e051fa81b1f005356b9a4fdee&oe=5B71D66D",
  cover_photo_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10625068_10204092899357192_8493302648326451673_n.jpg?_nc_cat=0&oh=0b479f1414a428164f07bbbb88f66f4e&oe=5B654792",
  bio: "Dreamer. Visionary. Artist",
- sex: "Female">
+ sex: "Female"
 )
 
 User.create(
@@ -76,5 +76,5 @@ User.create(
  profile_img_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-1/c90.1.1507.1507/1540444_10152757531844626_4986869029706754556_o.jpg?_nc_cat=0&oh=559ef82831fcdf6a9f05542866421856&oe=5B5E1416",
  cover_photo_url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/396226_10150619821354626_1958628994_n.jpg?_nc_cat=0&oh=2167e83cb469c053c711e0c3fdf8d2bf&oe=5B711CD0",
  bio: "Challenge Accepted.",
- sex: "Male">
+ sex: "Male"
 )
